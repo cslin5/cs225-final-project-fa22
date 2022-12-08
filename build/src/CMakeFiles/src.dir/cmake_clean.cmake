58ea89bbd843047522e087d1163f0326724effc9
Natalia Raymundi Pinheiro <nraym2@illinois.edu>
2022-12-08 23:42:47 +0000

    initial commit

---
+++ build/src/CMakeFiles/src.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/src.dir/algorithms.cpp.o"
+  "CMakeFiles/src.dir/algorithms.cpp.o.d"
+  "CMakeFiles/src.dir/transit.cpp.o"
+  "CMakeFiles/src.dir/transit.cpp.o.d"
+  "libsrc.a"
+  "libsrc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
